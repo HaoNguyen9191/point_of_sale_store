ae402bffdac6e043a89ae4093ac564efa1ec9f45
Nguyễn Đức Nhật Hào <thaingoc924282@gmail.com>
2025-03-04 20:10:36 +0700

    Update version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4acb3b3ef6d3ad380d1c5a40f01a0be7
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "6302e7958bfc17a41f894f9c59e9334c",
+"flutter_bootstrap.js": "d5b432583e77d3746c06498a1cee39b5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
